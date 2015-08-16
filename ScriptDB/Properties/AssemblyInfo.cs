020b10565a693d3461462b04cdf0ee46061e8e29
Adam Coyne <github@mail2.coyne.nu>
2015-08-16 16:57:44 -0500

    Bump to version 2

--- ScriptDB/Properties/AssemblyInfo.cs
+++ ScriptDB/Properties/AssemblyInfo.cs
@@ -28,35 +28,12 @@
 */
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("ScriptDb")]
-[assembly: AssemblyDescription("use me for generating sql scripts for any SQL Server 2000 or 2005 database.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Elsasoft")]
+[assembly: AssemblyDescription("SQL Server schema and data scripter")]
+[assembly: AssemblyCompany("Elsasoft LLC; Coyne Solutions Inc.")]
 [assembly: AssemblyProduct("ScriptDb")]
-[assembly: AssemblyCopyright("Copyright ©  2006")]
-[assembly: AssemblyTrademark("Copyright ©  2006")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM componenets.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyCopyright("Copyright © 2006 - 2015")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1e5bd6b9-c920-421e-9ad9-637112939646")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
